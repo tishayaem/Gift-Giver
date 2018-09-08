627af92e541840147ab2ffaf7f6ff5a56fd19caf
tishayaem <kostiantynkalinin@gmail.com>
2018-09-08 23:16:17 +0100

    Create gift component, initialised with person and present in state

---
+++ src/components/Gift.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class extends React.Component {
+    constructor() {
+        super();
+        this.state = {person: '', present: ''};
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
